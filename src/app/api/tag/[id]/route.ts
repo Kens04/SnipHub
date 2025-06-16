f966011f0bb60776ca2e8482b489a38b6d137199
Kens04 <kento090403@icloud.com>
2025-06-16 23:23:00 +0900

    タグAPIのGETエンドポイントにスニペット情報を追加

--- src/app/api/tag/[id]/route.ts
+++ src/app/api/tag/[id]/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const GET = async (
+export const DELETE = async (
   request: NextRequest,
   { params }: { params: { id: string } }
 ) => {
@@ -13,6 +13,30 @@ export const GET = async (
     const tag = await prisma.tag.findUnique({
       where: {
         id: parseInt(id),
+      },
+    });
+
+    return NextResponse.json({ status: "OK", tag }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
+
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const { name } = await request.json();
+
+  try {
+    const tag = await prisma.tag.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name,
       },
     });
 
--- src/app/api/tag/route.ts
+++ src/app/api/tag/route.ts
@@ -33,6 +33,48 @@ export async function GET() {
       select: {
         id: true,
         name: true,
+        snippetTags: {
+          select: {
+            snippet: {
+              select: {
+                id: true,
+                userId: true,
+                title: true,
+                description: true,
+                user: {
+                  select: {
+                    id: true,
+                    userName: true,
+                    iconUrl: true,
+                  },
+                },
+                category: {
+                  select: {
+                    id: true,
+                    name: true,
+                  },
+                },
+                tags: {
+                  select: {
+                    tag: {
+                      select: {
+                        id: true,
+                        name: true,
+                      },
+                    },
+                  },
+                },
+                likes: {
+                  select: {
+                    id: true,
+                    userId: true,
+                    snippetId: true,
+                  },
+                },
+              },
+            },
+          },
+        },
       },
     });
 
