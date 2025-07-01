e3fce93a2673ce27add40fe01d64381b8690307f
Kens04 <kento090403@icloud.com>
2025-07-01 22:45:22 +0900

    Headerコンポーネントを更新し、ユーザー情報の取得機能を追加しました。ユーザーがログインしている場合はアバターを表示し、そうでない場合は新規登録とログインリンクを表示します。また、スタイルを微調整しました。

--- src/app/_layout/Header.tsx
+++ src/app/_layout/Header.tsx
@@ -1,23 +1,60 @@
-import Link from "next/link";
-import React from "react";
-import { FaCode } from "react-icons/fa";
+"use client";
 
-export const Header: React.FC = () => {
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { FaCode } from "react-icons/fa";
+import Image from "next/image";
+import { useSupabaseSession } from "../_hooks/useSupabaseSession";
+import avatar from "../public/images/avatar.png";
+
+export interface User {
+  userName: string;
+  iconUrl: string;
+}
+
+export const Header = () => {
+  const { token, session } = useSupabaseSession();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (!session?.user.id) return;
+      const res = await fetch(`/api/user/${session.user.id}`);
+      if (res.ok) {
+        const data = await res.json();
+        setUser(data);
+      }
+    };
+    if (token) fetchUser();
+  }, [token, session]);
+
   return (
-    <header className="bg-white text-[#333] p-6 font-bold flex justify-between items-center fixed z-[1] w-full top-0 left-0">
+    <header className="bg-white text-[#333] px-6 py-4 font-bold flex justify-between items-center fixed z-[1] w-full top-0 left-0">
       <Link href="/" className="flex items-center gap-2">
-        <FaCode className="text-color-primary w-8 h-8" />
-        <span className="text-4xl">SnipHub</span>
+        <FaCode className="text-color-primary w-6 h-6" />
+        <span className="text-3xl">SnipHub</span>
       </Link>
-      <div className="flex gap-4 items-center justify-center">
-        <Link href="/signup">新規登録</Link>
-        <Link
-          href="/login"
-          className="text-white bg-color-primary hover:bg-color-primary-hover font-bold rounded-lg text-sm px-5 py-2.5 text-center"
-        >
-          ログイン
-        </Link>
-      </div>
+      {token ? (
+        <div className="flex gap-4 items-center justify-center">
+          <Image
+            className="w-[50px] h-[50px] rounded-full m-auto"
+            src={user?.iconUrl || avatar}
+            width={50}
+            height={50}
+            alt="avatar"
+          />
+        </div>
+      ) : (
+        <div className="flex gap-4 items-center justify-center">
+          <Link href="/signup">新規登録</Link>
+          <Link
+            href="/login"
+            className="text-white bg-color-primary hover:bg-color-primary-hover font-bold rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            ログイン
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
