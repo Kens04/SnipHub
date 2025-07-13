87eb06c181c4ba94873389ca459d834fefc7243f
Kens04 <kento090403@icloud.com>
2025-07-13 15:14:57 +0900

    DashboardLayoutコンポーネントを新規作成し、サイドバーとメインエリアを実装

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Sidebar } from "../_layout/Sidebar";
+import { useRouteGuard } from "./_hooks/useRouteGuard";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useRouteGuard();
+
+  return (
+    <>
+      {/* サイドバー */}
+      <Sidebar />
+
+      {/* メインエリア */}
+      <div className="mt-[88px] md:ml-[280px] md:mt-[100px] p-4 md:px-8 md:py-20">
+        {children}
+      </div>
+    </>
+  );
+}
