b898861e4c5aab8a1cf5b1920ac4540fa02732b6
Kens04 <kento090403@icloud.com>
2026-01-03 17:57:32 +0900

    SnippetsDataインターフェースを更新し、SnippetDataインターフェースを追加してデータ構造を整理

--- src/app/_types/snippet.ts
+++ src/app/_types/snippet.ts
@@ -1,16 +1,63 @@
-export interface SnippetData {
+export interface SnippetsData {
   snippets: {
     tags: {
-      tag: { name: string };
+      tag: {
+        id: number;
+        name: string;
+      };
     }[];
     category: {
+      id: number;
       name: string;
     };
     name: string;
     description: string;
+    contentBlocks: {
+      id: number;
+      content: string | null;
+      order: number;
+      preview?: {
+        template?: string;
+        code: string;
+        files?: Record<string, { code: string }>;
+      };
+      type: "markdown" | "text" | "preview";
+    }[];
     id: number;
     isPublic: boolean;
     title: string;
     updatedAt: string;
   }[];
+}
+
+export interface SnippetData {
+  snippet: {
+    tags: {
+      tag: {
+        id: number;
+        name: string;
+      };
+    }[];
+    category: {
+      id: number;
+      name: string;
+    };
+    name: string;
+    description: string;
+    contentBlocks: {
+      id: number;
+      content: string | null;
+      order: number;
+      preview?: {
+        template?: string;
+        code: string;
+        files?: Record<string, { code: string }>;
+      };
+      type: "markdown" | "text" | "preview";
+    }[];
+    id: number;
+    isPublic: boolean;
+    title: string;
+    updatedAt: string;
+  };
 }
